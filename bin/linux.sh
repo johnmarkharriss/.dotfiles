98e62e80be75cba0619f1528a6740ad3b8f742f1
John <jh34466@gmail.com>
2023-04-06 17:10:45 -0400

    Created linux shell setup scripts.

--- bin/linux.sh
+++ bin/linux.sh
@@ -6,12 +6,13 @@ if [ "$(uname)" != "Linux" ]; then
 fi
 # Creates a Trash Directory
 mkdir -p ~/TRASH
+#show hidden files
+ls -a
 #If .vimrc exists in the home directory, changes .vimrc name
 if [ -f "~./vimrc" ];then
 	echo ".vimrc changed to bup_vimrc" >> linuxsetup.log
 	mv .vimrc .bup_vimrc
 fi
-
 ./etc/vimrc > ~/.vimrc
 
 echo "source ~/.dotfiles/etc/bashrc_custom" >> ~/.bashrc
